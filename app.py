1a7fbdf562a762c699389acf95d6714601b18ab5
pwrignall <pwrignall@users.noreply.github.com>
2026-03-22 12:09:16 +0000

    Use TMDB link instead

--- app.py
+++ app.py
@@ -381,7 +381,6 @@ def api_watchlist():
             'release_date': movie.get('release_date', ''),
             'vote_average': movie.get('vote_average', 0),
             'genre_ids': movie.get('genre_ids', []),
-            'imdb_id': movie.get('imdb_id', ''),
             'genres': genre_names,
             'providers': {}  # Empty, will be loaded separately
         })
